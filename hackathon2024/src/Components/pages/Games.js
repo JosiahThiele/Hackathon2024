92f905fe2a48d0a720ada3049973f710e931d97e
JosiahThiele <josiahthiele@gmail.com>
2024-03-02 15:39:33 -0600

    TicTacToe

--- hackathon2024/src/Components/pages/Games.js
+++ hackathon2024/src/Components/pages/Games.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import '../../App.css'
+import TicTacToe from '../TicTacToe'
 
 export default function Games(){
   return (
     <>
       <header className="game-header">Games Page Header</header>
+
+      <div>
+        <TicTacToe/>
+      </div>
     </>
   )
 }
