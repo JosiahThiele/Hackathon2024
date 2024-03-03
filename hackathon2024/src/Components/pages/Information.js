958405ff18a2d0c707f83d0a2b4c4630480273de
Jacob Minder <jakewminder@gmail.com>
2024-03-02 21:50:59 -0600

    Added the Footer

--- hackathon2024/src/Components/pages/Information.js
+++ hackathon2024/src/Components/pages/Information.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../../App.css';
+import Footer from '../Footer'
 
 export default function Information() {
   return (
-    <div className="information-container">
-      <div className="information-box">
-        <h1>INFORMATION</h1>
-        <p>
-          Your bunch of text goes here. Your bunch of text goes here.
-          Your bunch of text goes here. Your bunch of text goes here.
-        </p>
+    <dv>
+      <div className="information-container">
+        <div className="information-box">
+          <h1>INFORMATION</h1>
+          <p>
+            Your bunch of text goes here. Your bunch of text goes here.
+            Your bunch of text goes here. Your bunch of text goes here.
+          </p>
+        </div>
       </div>
-    </div>
+      <Footer/>
+    </dv>
   );
 }
